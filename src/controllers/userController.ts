cee62c652d9d132e0c578a79ec29db1069e7b988
Bryan Cruz <bryan.oficio@gmail.com>
2021-02-27 23:46:47 -0300

    criado o primeiro repository

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,22 +1,22 @@
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/userModel';
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/userRepository";
 
 class UserController {
   async createUser(req: Request, res: Response) {
     const { name, email } = req.body;
-    const userRepository = getRepository(User);
+    const userRepo = getCustomRepository(UserRepository);
 
-    const userAlreadyExists = await userRepository.findOne({ email });
-    if(userAlreadyExists){
-      res.status(400).json({
-        error:'user already exists'
-      })
+    const userAlreadyExists = await userRepo.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        error: "user already exists",
+      });
     }
 
-    const user = userRepository.create({ name, email });
-    await userRepository.save(user);
-    res.json(user);
+    const user = userRepo.create({ name, email });
+    await userRepo.save(user);
+    return res.status(201).json(user);
   }
 }
 
