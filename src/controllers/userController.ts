7dafbe54e68ebed83c6f524694cd6e64f1398b8a
Bryan Henrique Cruz Souza <bryan.oficio@gmail.com>
2021-04-12 00:45:19 -0300

    usando a lib yup para validar campos do usuario

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,16 +1,29 @@
-import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { UserRepository } from "../repositories/userRepository";
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/userRepository';
+import * as yup from 'yup';
 
 class UserController {
   async createUser(req: Request, res: Response) {
     const { name, email } = req.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate({ name, email },{abortEarly:false});  
+    } catch (error) {
+      res.status(400).json({errors:error.errors});
+    }
+
     const userRepo = getCustomRepository(UserRepository);
 
     const userAlreadyExists = await userRepo.findOne({ email });
     if (userAlreadyExists) {
       return res.status(400).json({
-        error: "user already exists",
+        error: 'user already exists',
       });
     }
 
